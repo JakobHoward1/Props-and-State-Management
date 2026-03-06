5ec47a2a9dd169471a69e6dde0ced7958c08d416
JakobHoward1 <jakob.howard@edu.sait.ca>
2026-03-06 15:32:36 -0700

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+// Jakob Howard - 000925699
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import ToDoList from './ToDoList';
+export default function Index() {
+  const [tasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+  return (
+    <View>
+      <ToDoList tasks={tasks} />
+    </View>
+  );
+}
